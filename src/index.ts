e494cc9fa8d16e3aa57b875f6d074e1f7f365dec
1Conan <me@1conan.com>
2018-07-19 13:14:04 +0000

    custom exec function with shell escape

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
+import { exec as execAsync } from 'child_process';
 import { existsSync } from 'fs';
+import shellEscape from 'shell-escape';
 
 export default class {
   constructor(options: IOptions) {
     if (!existsSync(options.bin)) throw new Error(`${options.bin} doesn't exist!`);
     if (!existsSync(options.bmPath)) throw new Error(`${options.bmPath} doesn't exist`);
   }
+}
+function exec(bin: string, argument: string[]): Promise<{stdout: string, stderr: string}> {
+  const args = shellEscape(argument);
+  const command: string = `${bin} ${args}`;
+  return new Promise((resolve, reject) => {
+    execAsync(command, (err, stdout, stderr) => {
+      if (err) reject(err);
+      resolve({ stdout, stderr });
+    });
+  });
 }
 
 interface IOptions {
